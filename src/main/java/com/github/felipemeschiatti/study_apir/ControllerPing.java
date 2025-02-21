10f191cef1e01167f756f83db3367eebe6467cf9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 22:47:49 -0300

    Adicionar controller-produto

--- src/main/java/com/github/felipemeschiatti/study_apir/ControllerPing.java
+++ src/main/java/com/github/felipemeschiatti/study_apir/ControllerPing.java
@@ -1,5 +1,27 @@
 package com.github.felipemeschiatti.study_apir;
 
-public class ControllerPing {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-}
+
+@RestController
+@RequestMapping("ping")
+public class ControllerPing {
+   
+    @GetMapping
+
+    public String ping(){
+        return "pong";
+    }
+
+
+    
+    }
+
+        
+
+
+    
+
+
